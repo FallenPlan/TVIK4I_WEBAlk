ee8a72b1bb0235d3fc17523b76fcdef14c13b888
FallenPlan <59539062+FallenPlan@users.noreply.github.com>
2021-10-12 21:31:40 +0200

    README.md
    Run created Tests and watch test coverage.

---
+++ TVIK4I_gyak5/dependency/src/main/java/hu/iit/webalk/dependency/DependencyImpl.java
@@ -0,0 +1,14 @@
+package hu.iit.webalk.dependency;
+
+public class DependencyImpl implements Dependency {
+
+    @Override
+    public void helpNoParameterNoReturnValue() {
+
+    }
+
+    @Override
+    public int helpNoParameterReturnValue() {
+        return 45;
+    }
+}
