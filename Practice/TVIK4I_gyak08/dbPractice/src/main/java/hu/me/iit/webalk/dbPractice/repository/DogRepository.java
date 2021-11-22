14b4fe0afdcae0e0fcedb834015bc8803359106c
FallenPlan <59539062+FallenPlan@users.noreply.github.com>
2021-11-23 00:39:56 +0100

    Delete Practice/TVIK4I_gyak08 directory

--- Practice/TVIK4I_gyak08/dbPractice/src/main/java/hu/me/iit/webalk/dbPractice/repository/DogRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.me.iit.webalk.dbPractice.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DogRepository extends PagingAndSortingRepository<Dog, Long> {
-    Iterable<Dog> findAllByAgeGreaterThanEqual(int age);
-}
