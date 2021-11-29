19d3c092ccc1a9491fa9c5cf75452f55234107be
FallenPlan <59539062+FallenPlan@users.noreply.github.com>
2021-11-29 23:12:22 +0100

    Add files via upload

---
+++ TVIK4I_gyak11/licitPractice/src/main/java/hu/me/iit/webalk/licitPractice/LicitPracticeApplication.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.licitPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitPracticeApplication.class, args);
+	}
+
+}
---
+++ TVIK4I_gyak11/licitPractice/src/test/java/hu/me/iit/webalk/licitPractice/LicitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.licitPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
