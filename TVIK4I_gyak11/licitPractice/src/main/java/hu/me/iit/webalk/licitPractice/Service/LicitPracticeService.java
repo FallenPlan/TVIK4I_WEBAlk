141c423825e65c62a26b982ea6e50f4008a19806
FallenPlan <59539062+FallenPlan@users.noreply.github.com>
2021-11-29 23:13:44 +0100

    Delete TVIK4I_gyak11/licitPractice directory

--- TVIK4I_gyak11/licitPractice/src/main/java/hu/me/iit/webalk/licitPractice/Service/LicitPracticeService.java
+++
@@ -1,7 +0,0 @@
-package hu.me.iit.webalk.licitPractice.Service;
-
-public interface LicitPracticeService {
-    int getLowest();
-    int getLowestForUser(int userId);
-    void saveNewLicit(int licit, int userId);
-}
